1f5aa8efe8f8d08814a372bdb74d63a81acb150d
Yuri0155 <35896547+Yuri0155@users.noreply.github.com>
2023-12-08 11:09:10 +0100

    hello2

--- Hello.cs
+++ Hello.cs
@@ -3,8 +3,7 @@ using System;
 class Program
 {
     static void Main()
-        {
-                Console.WriteLine("Hello, World!");
-                    }
-                    }
-                    ```
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
