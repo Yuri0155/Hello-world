d5eece4be9a9a1b12ce74f8e030663deea8e719c
Yuri0155 <maroskoteles@gmail.com>
2024-01-12 12:28:51 +0100

    a

---
+++ ConsoleApp1/Vetva.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Vetva
+    {
+        public Vetva(double a, double b)
+        {
+            polomer = a;
+            vyska = b;
+        }
+
+        public double vyska;
+        public double polomer;
+    }
+}
